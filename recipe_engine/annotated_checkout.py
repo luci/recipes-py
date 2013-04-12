ac570502a23a0073c73f96f1940e3105409a4cdc
agable@chromium.org <agable@chromium.org>
2013-04-12 04:18:21 +0000

    Run annotated_checkout git commands from the git directory.
    R=iannucci@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/13998004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@193843 0039d316-1c4b-4281-b951-d872f2087c98

--- recipe_engine/annotated_checkout.py
+++ recipe_engine/annotated_checkout.py
@@ -194,10 +194,9 @@ class GitCheckout(Checkout):
     self.run_git('update-ref', 'refs/heads/%s' % branch, 'origin/%s' % branch)
 
   def run_git(self, *cmd):
-    cmd = ('git', '--git-dir', os.path.join(self.cwd, '.git'),
-           '--work-tree', self.cwd) + cmd
+    cmd = ('git',) + cmd
     print 'Running: %s' % (' '.join(pipes.quote(x) for x in cmd))
-    subprocess.check_call(cmd)
+    subprocess.check_call(cmd, cwd=self.cwd)
 
   def clean(self):
     self.run_git('clean', '-f', '-d', '-x')
