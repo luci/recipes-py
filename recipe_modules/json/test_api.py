c97378824835e8e95bf82bb91bb4f5174e32d72c
iannucci@chromium.org <iannucci@chromium.org>
2013-10-03 00:09:35 +0000

    Revert "Do a minimal pass of tests before committing to a full round."
    This reverts commit bf650255ac38899a276f9088e9fb21f49f42dd24.
    
    Revert "Fix layout_test_wrapper when not called with any --test-list"
    
    This reverts commit 6ca8db334b3bf71b2b7c3510cda5a13ccc29caa3.
    
    R=agable@chromium.org
    BUG=265271
    
    Review URL: https://chromiumcodereview.appspot.com/25474007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@226625 0039d316-1c4b-4281-b951-d872f2087c98

--- recipe_modules/json/test_api.py
+++ recipe_modules/json/test_api.py
@@ -14,25 +14,19 @@ class JsonTestApi(recipe_test_api.RecipeTestApi):
   def test_results(self, test_results, retcode=None):
     return self.output(test_results.as_jsonish(), retcode)
 
-  def canned_test_output(self, passing, minimal=False, passes=9001):
+  def canned_test_output(self, good, passes=9001):
     """Produces a 'json test results' compatible object with some canned tests.
     Args:
-      passing - Determines if this test result is passing or not.
+      good - Determines if this test result is passing or not.
       passes - The number of (theoretically) passing tests.
-      minimal - If True, the canned output will omit one test to emulate the
-                effect of running fewer than the total number of tests.
     """
-    if_failing = lambda fail_val: None if passing else fail_val
+    bad = lambda fail_val: None if good else fail_val
     t = TestResults()
     t.raw['num_passes'] = passes
-    t.add_result('flake/totally-flakey.html', 'PASS',
-                 if_failing('TIMEOUT PASS'))
-    t.add_result('tricky/totally-maybe-not-awesome.html', 'PASS',
-                 if_failing('FAIL'))
-    t.add_result('bad/totally-bad-probably.html', 'PASS',
-                 if_failing('FAIL'))
-    if not minimal:
-      t.add_result('good/totally-awesome.html', 'PASS')
+    t.add_result('good/totally-awesome.html', 'PASS')
+    t.add_result('flake/totally-flakey.html', 'PASS', bad('TIMEOUT PASS'))
+    t.add_result('tricky/totally-maybe-not-awesome.html', 'PASS', bad('FAIL'))
+    t.add_result('bad/totally-bad-probably.html', 'PASS', bad('FAIL'))
     ret = self.test_results(t)
-    ret.retcode = 0 if passing else 1
+    ret.retcode = 0 if good else 1
     return ret
