1526148c7ecc691d662efb88c142c8451b0d11b0
pvalenzuela@chromium.org <pvalenzuela@chromium.org>
2014-12-04 19:14:51 +0000

    Create UUID recipe_module
    This module allows the generation of random UUIDs from recipes.
    
    BUG=NONE
    R=nodir@chromium.org
    
    Review URL: https://codereview.chromium.org/779773003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@293238 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ recipe_modules/uuid/api.py
@@ -0,0 +1,16 @@
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import uuid
+
+from slave import recipe_api
+
+class UuidApi(recipe_api.RecipeApi):
+  def random(self):
+    """Generates and returns a random UUID as a string."""
+    if self._test_data.enabled:
+      return '00000000-0000-0000-0000-000000000000'
+    else: # pragma: no cover
+      return str(uuid.uuid4())
+
