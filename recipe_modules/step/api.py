730cbb7ac2f5dd30e3b3b715e3115b5773e7eca7
kjellander@chromium.org <kjellander@chromium.org>
2015-04-24 12:08:30 +0000

    Revert of Re^3land of 'Make subannotations more robust' (patchset #5 id:80001 of https://codereview.chromium.org/1080283003/)
    Reason for revert:
    This breaks our Windows perf bot exactly as last time I reverted this (in https://codereview.chromium.org/1072823002).
    
    Build failures:
    http://build.chromium.org/p/client.webrtc/builders/Win32%20Release%20%5Blarge%20tests%5D/builds/4210
    http://build.chromium.org/p/client.webrtc/builders/Win32%20Release%20%5Blarge%20tests%5D/builds/4211
    http://build.chromium.org/p/client.webrtc/builders/Win32%20Release%20%5Blarge%20tests%5D/builds/4212
    http://build.chromium.org/p/client.webrtc/builders/Win32%20Release%20%5Blarge%20tests%5D/builds/4213
    
    Please at least monitor that bot for the next re-land. I'd be interested in knowing what's causing this and if we're doing something wrong. AFAIK there are no hacks involved in that bot's perf setup (except that it's Windows, which always is tricky).
    
    Original issue's description:
    > Re^3land of 'Make subannotations more robust'
    > 
    > Re^0land: https://codereview.chromium.org/1001183002/
    > Re^1land: https://codereview.chromium.org/1076643002/
    > Re^2land: https://chromiumcodereview.appspot.com/1075053002/
    > 
    > This patch makes the recipe engine parse and re-emit subannotations when allow_subannotations is True.  This means that any of the subannotator's steps which are duplicates of other steps executed will be disambiguated.  When an allow_subannotations step is actually emits annotations, an (end) step is emitted after it is done to have something to hang metadata on (it also conveniently communicates when we re-enter recipes).
    > 
    > BUG=466409
    > TBR=iannucci@chromium.org
    > 
    > Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=294973
    
    TBR=iannucci@chromium.org,luqui@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=466409
    
    Review URL: https://codereview.chromium.org/1108573002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@294976 0039d316-1c4b-4281-b951-d872f2087c98

--- recipe_modules/step/api.py
+++ recipe_modules/step/api.py
@@ -9,6 +9,10 @@ from slave import recipe_util
 # Inherit from RecipeApiPlain because the only thing which is a step is
 # run_from_dict()
 class StepApi(recipe_api.RecipeApiPlain):
+  def __init__(self, **kwargs):
+    super(StepApi, self).__init__(**kwargs)
+    self._step_names = {}
+
   EXCEPTION = 'EXCEPTION'
   FAILURE = 'FAILURE'
   SUCCESS = 'SUCCESS'
@@ -96,9 +100,13 @@ class StepApi(recipe_api.RecipeApiPlain):
 
     command = list(wrapper)
     command += cmd
-
     compositor = recipe_api._STEP_CONTEXT
     kwargs['cmd'] = command
+    step_count = self._step_names.setdefault(name, 0) + 1
+    self._step_names[name] = step_count
+    if step_count > 1:
+      name = "%s (%d)" % (name, step_count)
+
     kwargs['ok_ret'] = ok_ret
     kwargs['infra_step'] = bool(infra_step)
 
