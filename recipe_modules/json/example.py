1131e2254b94e0a365f352d87f023fb6e159f75a
machenbach@chromium.org <machenbach@chromium.org>
2015-05-19 08:08:52 +0000

    Add more examples to json recipe module
    BUG=
    
    Review URL: https://codereview.chromium.org/1134353004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@295305 0039d316-1c4b-4281-b951-d872f2087c98

--- recipe_modules/json/example.py
+++ recipe_modules/json/example.py
@@ -9,9 +9,15 @@ DEPS = [
 
 
 def GenSteps(api):
-  step_result = api.step('echo', ['echo', '[1, 2, 3]'],
+  step_result = api.step('echo1', ['echo', '[1, 2, 3]'],
       stdout=api.json.output())
   assert step_result.stdout == [1, 2, 3]
+
+  # Example demonstrating the usage of step_test_data for json stdout.
+  step_result = api.step('echo2', ['echo', '[2, 3, 4]'],
+      step_test_data=lambda: api.json.test_api.output_stream([2, 3, 4]),
+      stdout=api.json.output())
+  assert step_result.stdout == [2, 3, 4]
 
   assert api.json.is_serializable('foo')
   assert not api.json.is_serializable(set(['foo', 'bar', 'baz']))
@@ -19,4 +25,4 @@ def GenSteps(api):
 
 def GenTests(api):
   yield (api.test('basic') +
-      api.step_data('echo', stdout=api.json.output([1, 2, 3])))
+      api.step_data('echo1', stdout=api.json.output([1, 2, 3])))
