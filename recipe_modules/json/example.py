7dafac4ae330dcdf9e0aa05eefc0b0c6f1b59fdc
machenbach@chromium.org <machenbach@chromium.org>
2015-07-17 13:17:10 +0000

    Add json recipe example with multiple output files.
    BUG=
    
    Review URL: https://codereview.chromium.org/1246433002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@296044 0039d316-1c4b-4281-b951-d872f2087c98

--- recipe_modules/json/example.py
+++ recipe_modules/json/example.py
@@ -4,6 +4,7 @@
 
 DEPS = [
   'json',
+  'python',
   'step',
 ]
 
@@ -22,7 +23,27 @@ def RunSteps(api):
   assert api.json.is_serializable('foo')
   assert not api.json.is_serializable(set(['foo', 'bar', 'baz']))
 
+  # Example demonstrating multiple json output files.
+  result = api.python.inline(
+      'foo',
+      """
+      import json
+      import sys
+      with open(sys.argv[1], 'w') as f:
+        f.write(json.dumps([1, 2, 3]))
+      with open(sys.argv[2], 'w') as f:
+        f.write(json.dumps([2, 3, 4]))
+      """,
+      args=[api.json.output(), api.json.output()],
+  )
+  assert result.json.output_all == [[1, 2, 3], [2, 3, 4]]
+
 
 def GenTests(api):
   yield (api.test('basic') +
-      api.step_data('echo1', stdout=api.json.output([1, 2, 3])))
+      api.step_data('echo1', stdout=api.json.output([1, 2, 3])) +
+      api.step_data(
+          'foo',
+          api.json.output([1, 2, 3]) +
+          api.json.output([2, 3, 4]),
+      ))
