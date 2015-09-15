0fb6f7402574afcc04c966dd01c73eaf051b3818
Luke Palmer <luqui@google.com>
2015-09-15 11:28:28 -0700

    Fix disambiguation to consider steps with different fully-qualified names different.
    This is a port of the following tools/build CL:
    https://codereview.chromium.org/1315253005
    
    BUG=
    CC=machenbach@chromium.org
    TBR=iannucci@chromium.org
    
    Review URL: https://codereview.chromium.org/1335943004 .

--- recipe_modules/step/api.py
+++ recipe_modules/step/api.py
@@ -121,19 +121,22 @@ class StepApi(recipe_api.RecipeApiPlain):
     command += cmd
     compositor = recipe_api._STEP_CONTEXT
     kwargs['cmd'] = command
-    step_count = self._step_names.setdefault(name, 0) + 1
-    self._step_names[name] = step_count
-    if step_count > 1:
-      name = "%s (%d)" % (name, step_count)
 
     kwargs['ok_ret'] = ok_ret
     kwargs['infra_step'] = bool(infra_step)
 
     # Obtain information from composite step parent.
-    kwargs['name'] = compositor.get_with_context('name', name)
+    name = compositor.get_with_context('name', name)
     kwargs['env'] = compositor.get_with_context('env', kwargs.get('env', {}))
     kwargs['step_nest_level'] = compositor.get_with_context('nest_level', 0)
     kwargs.setdefault('cwd', self.m.path['slave_build'])
+
+    # Disambiguate repeated names
+    step_count = self._step_names.setdefault(name, 0) + 1
+    self._step_names[name] = step_count
+    if step_count > 1:
+      name = "%s (%d)" % (name, step_count)
+    kwargs['name'] = name
 
     schema = self.make_config()
     schema.set_val(kwargs)
