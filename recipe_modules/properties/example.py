c55462b5c2458036648d53e6a4d923a63ed8b7cc
tandrii <tandrii@chromium.org>
2016-04-20 04:36:36 -0700

    Add tryserver_gerrit test properties.
    R=phajdan.jr@chromium.org,martiniss@chromium.org
    BUG=604735
    
    Review URL: https://codereview.chromium.org/1895783005

--- recipe_modules/properties/example.py
+++ recipe_modules/properties/example.py
@@ -48,3 +48,6 @@ def GenTests(api):
          api.properties.git_scheduled(test_prop=None))
   yield (api.test('buildbot_tryserver') +
          api.properties.tryserver(test_prop=None))
+  yield (api.test('buildbot_tryserver_gerrit') +
+         api.properties.tryserver_gerrit(full_project_name='infra/infra',
+                                         test_prop=None))
--- recipe_modules/properties/test_api.py
+++ recipe_modules/properties/test_api.py
@@ -72,3 +72,33 @@ class PropertiesTestApi(recipe_test_api.RecipeTestApi):
     )
     ret.properties.update(kwargs)
     return ret
+
+  def tryserver_gerrit(self, full_project_name, gerrit_host=None, **kwargs):
+    """
+    Merge kwargs into a typical buildbot properties blob for a job fired off
+    by a gerrit tryjob on the tryserver, and return the blob.
+
+    Arguments:
+      full_project_name: (required) name of the project in Gerrit.
+      gerrit_host: hostname of the gerrit server.
+    """
+    gerrit_host = gerrit_host or 'chromium.googlesource.com'
+    ret = self.generic(
+        branch='',
+        category='cq',
+        gerrit='https://%s' % gerrit_host,
+        patch_storage='gerrit',
+        project=None,
+        reason='CQ',
+        repository='https://%s/%s' % (gerrit_host, full_project_name),
+        requester='commit-bot@chromium.org',
+        revision='HEAD',
+    )
+    ret.properties.update({
+        'event.change.id': u'infra%2Finfra~master~Ideadbeaf',
+        'event.change.number': 338811,
+        'event.change.url': u'https://chromium-review.googlesource.com/#/c/338811',
+        'event.patchSet.ref': u'refs/changes/11/338811/3',
+    })
+    ret.properties.update(kwargs)
+    return ret
