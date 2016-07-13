3de962bcccb8eac10810d7247618c5c9089b8fe7
tandrii <tandrii@chromium.org>
2016-07-13 06:53:15 -0700

    shutil.rmtree: transition everything to new implementation.
    It has been battle-tested in V8.
    
    BUG=584783
    R=phajdan.jr@chromium.org,martiniss@chromium.org
    
    Review-Url: https://codereview.chromium.org/2152443002

--- recipe_modules/shutil/api.py
+++ recipe_modules/shutil/api.py
@@ -7,20 +7,11 @@ from recipe_engine import recipe_api
 class ShutilApi(recipe_api.RecipeApi):
   def rmtree(self, path, **kwargs):
     name = kwargs.pop('name', 'rmtree %s' % path)
-    if kwargs.pop('flag_smart_rmtree', False):
-      # TODO(tandrii): remove this flag and makes this the only implementation.
-      self.m.python(
-          name,
-          self.resource('rmtree.py'),
-          args=[path],
-          **kwargs)
-      return
-    self.m.python.inline(
+    # TODO(tandrii): remove this flag.
+    kwargs.pop('flag_smart_rmtree', False)
+    self.m.python(
         name,
-        """
-          import shutil, sys
-          shutil.rmtree(sys.argv[1])
-        """,
+        self.resource('rmtree.py'),
         args=[path],
         **kwargs)
 
