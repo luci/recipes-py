d6ceb66a08508d430a0fbdd5a6a17c09b9633694
phajdan.jr <phajdan.jr@chromium.org>
2017-04-12 01:19:19 -0700

    Enable strict coverage for step module (reland #1)
    This is a reland of https://codereview.chromium.org/2806363004/
    after fixing swarming recipe module (https://chromium-review.googlesource.com/c/474909).
    
    TBR=iannucci
    
    BUG=chromium:693058
    
    Review-Url: https://codereview.chromium.org/2809403002

--- recipe_modules/step/tests/subannotations.py
+++ recipe_modules/step/tests/subannotations.py
@@ -6,7 +6,9 @@ DEPS = ['step']
 
 def RunSteps(api):
   api.step('Dont subannotate me', ['echo', '@@@BUILD_STEP@steppy@@@'])
-  api.step('Subannotate me', ['echo', '@@@BUILD_STEP@pippy@@@'], allow_subannotations=True)
+  api.step('Subannotate me',
+           ['echo', '@@@BUILD_STEP@pippy@@@'],
+           allow_subannotations=True)
 
 def GenTests(api):
   yield api.test('basic')
