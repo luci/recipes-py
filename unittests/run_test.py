bf2ff828987549b946135919bd1bffd6e42fd8fd
iannucci <iannucci@chromium.org>
2017-05-09 10:38:16 -0700

    [doc/recipes.py] Add option to recipes.py to allow it to work without .git
    This will allow the per-repo recipes.py script to work with isolated
    copies of repos (which lack .git directories).
    
    This also makes the bootstrap script not duplicate the --package option if it's
    passed.
    
    R=borenet@google.com, dnj@chromium.org, phajdan.jr@chromium.org
    BUG=
    
    Review-Url: https://codereview.chromium.org/2872663003

---
+++ unittests/run_test.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# Copyright 2016 The LUCI Authors. All rights reserved.
+# Use of this source code is governed under the Apache License, Version 2.0
+# that can be found in the LICENSE file.
+
+import os
+import shutil
+import subprocess
+import sys
+import unittest
+
+import repo_test_util
+
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+class RunTest(repo_test_util.RepoTest):
+  def test_run(self):
+    repos = self.repo_setup({'a': []})
+    self.update_recipe_module(repos['a'], 'mod', {'foo': []})
+    self.update_recipe(repos['a'], 'a_recipe', ['mod'], [('mod', 'foo')])
+    try:
+      subprocess.check_output([
+        sys.executable, os.path.join(repos['a']['root'], 'recipes.py'),
+        '-v', '-v', 'run', 'a_recipe',
+      ], stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as ex:
+      print >> sys.stdout, ex.message, ex.output
+      raise
+
+  def test_run_no_git(self):
+    repos = self.repo_setup({'a': []})
+    self.update_recipe_module(repos['a'], 'mod', {'foo': []})
+    self.update_recipe(repos['a'], 'a_recipe', ['mod'], [('mod', 'foo')])
+    shutil.rmtree(os.path.join(repos['a']['root'], '.git'))
+    shutil.copy(os.path.join(ROOT_DIR, 'doc', 'recipes.py'),
+                os.path.join(repos['a']['root'], 'recipes.py'))
+    try:
+      subprocess.check_output([
+        sys.executable, os.path.join(repos['a']['root'], 'recipes.py'),
+        '-v', '-v',
+        '--package', os.path.join(repos['a']['root'], 'infra', 'config',
+                                  'recipes.cfg'),
+        'run', 'a_recipe',
+      ], stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as ex:
+      print >> sys.stdout, ex.message, ex.output
+      raise
+
+
+if __name__ == '__main__':
+  sys.exit(unittest.main())
+
