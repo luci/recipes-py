4d872b88fb06006ec917c6d2a3324e26c0124319
Vadim Shtayura <vadimsh@chromium.org>
2017-10-03 17:30:20 -0700

    Add 'service_account' recipe module.
    It can be used to grab OAuth tokens for task associated accounts on LUCI.
    Depends on 'authutil' present in PATH.
    
    Supersedes https://cs.chromium.org/chromium/build/scripts/slave/recipe_modules/service_account/
    
    R=iannucci@chromium.org
    BUG=731854, 770880
    
    Change-Id: Iab51bdbadade0831f458507609104afaa310da77
    Reviewed-on: https://chromium-review.googlesource.com/698924
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>

---
+++ recipe_modules/service_account/examples/full.py
@@ -0,0 +1,57 @@
+# Copyright 2017 The LUCI Authors. All rights reserved.
+# Use of this source code is governed under the Apache License, Version 2.0
+# that can be found in the LICENSE file.
+
+from recipe_engine.recipe_api import Property
+
+DEPS = [
+  'platform',
+  'properties',
+  'raw_io',
+  'service_account',
+  'path',
+]
+
+PROPERTIES = {
+  'key_path': Property(),
+  'scopes': Property(),
+  'lifetime_sec': Property(),
+}
+
+
+def RunSteps(api, key_path, scopes, lifetime_sec):
+  if key_path:
+    account = api.service_account.from_credentials_json(key_path)
+  else:
+    account = api.service_account.default()
+  account.get_access_token(scopes, lifetime_sec)
+
+
+def GenTests(api):
+  def props(key_path=None, scopes=None, lifetime_sec=None):
+    return api.properties.generic(
+        key_path=key_path,
+        scopes=scopes,
+        lifetime_sec=lifetime_sec)
+
+  yield (
+      api.test('default') +
+      api.platform('linux', 64) +
+      props())
+  yield (
+      api.test('windows') +
+      api.platform('win', 64) +
+      props())
+  yield (
+      api.test('json_key') +
+      api.platform('linux', 64) +
+      props(key_path=api.path['start_dir'].join('key_name.json')))
+  yield (
+      api.test('custom_scopes_and_lifetime') +
+      api.platform('linux', 64) +
+      props(scopes=['B', 'A'], lifetime_sec=300))
+  yield (
+      api.test('no_authutil') +
+      api.platform('linux', 64) +
+      props() +
+      api.step_data('get access token for default account', retcode=1))
