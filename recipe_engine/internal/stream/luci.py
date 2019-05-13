f34152acbe36ba6b4dc513885271cfca8838a634
Raul Tambre <raul@tambre.ee>
2019-05-13 17:25:37 +0000

    Fix invalid Python file encoding "utf8"
    Since Python 3 it must be "utf-8", which is the official name.
    This is backwards compatible with Python 2.
    
    Bug: 962277
    Change-Id: I00f4c8a414ae93aa268664b5fa3b6d5e1fca1f05
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/recipes-py/+/1607804
    Auto-Submit: Raul Tambre <raul@tambre.ee>
    Commit-Queue: Robbie Iannucci <iannucci@chromium.org>
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>

--- recipe_engine/internal/stream/luci.py
+++ recipe_engine/internal/stream/luci.py
@@ -1,4 +1,4 @@
-# -*- coding: utf8 -*-
+# -*- coding: utf-8 -*-
 # Copyright 2019 The LUCI Authors. All rights reserved.
 # Use of this source code is governed under the Apache License, Version 2.0
 # that can be found in the LICENSE file.
