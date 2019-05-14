55fabf49b398f40fe4561f37ca5bac0eb9352e7e
Robert Iannucci <iannucci@chromium.org>
2019-05-14 20:37:45 +0000

    Move simulator stream engine to its own file.
    R=tandrii@chromium.org
    
    Bug: 910369
    Change-Id: Id08d9188cd51209e37cff7dcb7782b13fc1300c2
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/recipes-py/+/1610708
    Auto-Submit: Robbie Iannucci <iannucci@chromium.org>
    Reviewed-by: Andrii Shyshkalov <tandrii@chromium.org>
    Commit-Queue: Robbie Iannucci <iannucci@chromium.org>

--- recipe_engine/internal/commands/test/cmd.py
+++ recipe_engine/internal/commands/test/cmd.py
@@ -37,11 +37,9 @@ from .... import config_types
 
 from ...engine import RecipeEngine
 from ...engine_env import FakeEnviron
-from ...exceptions import CrashEngine
 from ...step_runner.sim import SimulationStepRunner
-from ...stream import StreamEngine
-from ...stream.annotator import AnnotatorStreamEngine
 from ...stream.invariants import StreamEngineInvariants
+from ...stream.simulator import SimulationAnnotatorStreamEngine
 from ...test import magic_check_fn
 
 from ..doc.cmd import regenerate_docs
@@ -1026,87 +1024,6 @@ def run_run(test_filter, jobs, json_file, mode):
 
   return rc
 
-
-class _NopFile(object):
-  # pylint: disable=multiple-statements,missing-docstring
-  def write(self, data): pass
-  def flush(self): pass
-
-
-class _NopLogStream(StreamEngine.Stream):
-  # pylint: disable=multiple-statements
-  def write_line(self, line): pass
-  def close(self): pass
-
-
-class _NopStepStream(AnnotatorStreamEngine.StepStream):
-  def __init__(self, engine, step_name):
-    super(_NopStepStream, self).__init__(engine, _NopFile(), step_name)
-
-  def new_log_stream(self, _):
-    return _NopLogStream()
-
-  def close(self):
-    pass
-
-
-class _SimulationStepStream(AnnotatorStreamEngine.StepStream):
-  # We override annotations we don't want to show up in followup_annotations
-  def new_log_stream(self, log_name):
-    # We sink 'execution details' to dev/null. This is the log that the recipe
-    # engine produces that contains the printout of the command, environment,
-    # etc.
-    #
-    # The '$debug' log is conditionally filtered in _merge_presentation_updates.
-    if log_name in ('execution details',):
-      return _NopLogStream()
-    return super(_SimulationStepStream, self).new_log_stream(log_name)
-
-  def trigger(self, spec):
-    pass
-
-  def close(self):
-    pass
-
-
-class SimulationAnnotatorStreamEngine(AnnotatorStreamEngine):
-  """Stream engine which just records generated commands."""
-
-  # TODO(iannucci): Move this (and related classes) to their own file, perhaps
-  # adjacent to AnnotatorStreamEngine.
-
-  def __init__(self):
-    self._step_buffer_map = collections.OrderedDict()
-    super(SimulationAnnotatorStreamEngine, self).__init__(
-        self._step_buffer(None))
-
-  @property
-  def buffered_steps(self):
-    """Returns an OrderedDict of all steps run by dot-name to a cStringIO
-    buffer with any annotations printed."""
-    return self._step_buffer_map
-
-  def _step_buffer(self, step_name):
-    return self._step_buffer_map.setdefault(step_name, cStringIO.StringIO())
-
-  def new_step_stream(self, step_config):
-    # TODO(iannucci): don't skip these. Omitting them for now to reduce the
-    # amount of test expectation changes.
-    steps_to_skip = (
-      'recipe result',   # explicitly covered by '$result'
-    )
-    if step_config.name in steps_to_skip:
-      return _NopStepStream(self, step_config.name)
-
-    stream = _SimulationStepStream(
-        self, self._step_buffer(step_config.name), step_config.name)
-    # TODO(iannucci): this is duplicated with
-    # AnnotatorStreamEngine._create_step_stream
-    if len(step_config.name_tokens) > 1:
-      # Emit our current nest level, if we are nested.
-      stream.output_annotation(
-          'STEP_NEST_LEVEL', str(len(step_config.name_tokens)-1))
-    return stream
 
 
 def handle_killswitch(*_):
