e7fec5ccb9e70bf17b7878e63052a874afa06856
Michael Moss <mmoss@google.com>
2019-08-13 18:51:45 +0000

    Change is_luci default to True.
    Most things should be on LUCI now, so this better reflects reality, and
    might help shakeout any obsolete non-LUCI handling.
    
    Bug: 993137
    Recipe-Nontrivial-Roll: build
    Recipe-Nontrivial-Roll: build_limited_scripts_slave
    Recipe-Nontrivial-Roll: chromiumos
    Recipe-Nontrivial-Roll: depot_tools
    Recipe-Nontrivial-Roll: infra
    Recipe-Nontrivial-Roll: release_scripts
    Recipe-Nontrivial-Roll: skia
    Change-Id: I5d43654d1f8ef2d980a3c8892e718b102e6cbbe1
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/recipes-py/+/1750109
    Auto-Submit: Michael Moss <mmoss@chromium.org>
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    Commit-Queue: Michael Moss <mmoss@chromium.org>

--- recipe_modules/runtime/api.py
+++ recipe_modules/runtime/api.py
@@ -21,7 +21,7 @@ class RuntimeApi(recipe_api.RecipeApi):
 
     Should be used only during migration from Buildbot to LUCI stack.
     """
-    return bool(self._properties.get('is_luci', False))
+    return bool(self._properties.get('is_luci', True))
 
   @property
   def is_experimental(self):
