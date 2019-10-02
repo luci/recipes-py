39baf65a40b3d08e7cd414493fe607eaf18637d0
Robert Iannucci <iannucci@chromium.org>
2019-10-02 11:00:37 +0000

    [futures:examples/background_helper] Speculatively switch to os._exit
    Hopefully this clears up the redness on the Windows builder.
    
    TBR=tandrii@chromium.org
    
    Bug: 910369
    Recipe-Nontrivial-Roll: fuchsia
    Change-Id: I0c84e06a3adad0c544f29eef02ee68f304c63119
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/recipes-py/+/1835651
    Commit-Queue: Robbie Iannucci <iannucci@chromium.org>
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    Auto-Submit: Robbie Iannucci <iannucci@chromium.org>

--- recipe_modules/futures/examples/background_helper.resources/helper.py
+++ recipe_modules/futures/examples/background_helper.resources/helper.py
@@ -12,7 +12,7 @@ def do_exit():
   print >>sys.stderr, ">> CAUGHT SIGNAL, CLEANING UP"
   time.sleep(2)
   print >>sys.stderr, ">> DONE; EXITING"
-  sys.exit(0)
+  os._exit(0)
 
 signal.signal(
     (
