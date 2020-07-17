c1b7a3041fc0dedb1ee46bcb206e0b048ce7a20f
Robert Iannucci <iannucci@chromium.org>
2020-07-17 17:40:23 +0000

    [raw_io] Make output_dir lazy.
    This will allow recipes to continue using output_dir but without
    exploding their memory usage.
    
    R=bpastene@chromium.org, yiwzhang@google.com
    
    Bug: 1085016
    Recipe-Nontrivial-Roll: build
    Recipe-Nontrivial-Roll: build_limited_scripts_slave
    Recipe-Nontrivial-Roll: chrome_release
    Change-Id: I2a569341dad3a1e14f44b57f3e91cd1b769ea7b5
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/recipes-py/+/2303595
    Auto-Submit: Robbie Iannucci <iannucci@chromium.org>
    Commit-Queue: Ben Pastene <bpastene@chromium.org>
    Reviewed-by: Ben Pastene <bpastene@chromium.org>
    Reviewed-by: Yiwei Zhang <yiwzhang@google.com>

---
+++ recipe_modules/raw_io/examples/full.resources/dump_files.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# Copyright 2020 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+import sys
+
+dump_dir = sys.argv[1]
+os.mkdir(os.path.join(dump_dir, 'some'))
+with open(os.path.join(dump_dir, 'some', 'file'), 'w') as f:
+  f.write('cool contents')
+with open(os.path.join(dump_dir, 'other_file'), 'w') as f:
+  f.write('whatever')
+
+# this is silly on non-windows, but we need pairity with the simulations here.
+os.mkdir(os.path.join(dump_dir, 'windows'))
+with open(os.path.join(dump_dir, 'windows', 'slash'), 'w') as f:
+  f.write('slashes are fixed')
--- recipe_modules/raw_io/test_api.py
+++ recipe_modules/raw_io/test_api.py
@@ -18,9 +18,21 @@ class RawIOTestApi(recipe_test_api.RecipeTestApi): # pragma: no cover
   @recipe_test_api.placeholder_step_data
   @staticmethod
   def output_dir(files_dict, retcode=None, name=None):
+    """Use to mock an `output_dir` placeholder.
+
+    Example:
+
+       yield api.test('tname') + api.step_data('sname', api.raw_io.output_dir({
+         "some/file": "contents of some/file",
+       }))
+    """
     assert type(files_dict) is dict
     assert all(type(key) is str for key in files_dict.keys())
     assert all(type(value) is str for value in files_dict.values())
+    files_dict = {
+      key.replace('\\', '/'): value
+      for key, value in files_dict.iteritems()
+    }
     return files_dict, retcode, name
 
   def stream_output(self, data, stream='stdout', retcode=None, name=None):
