3e7ed54f9b9aeef3aeb254182b62744f52ac824c
Rob Mohr <mohrr@google.com>
2021-09-02 23:46:55 +0000

    [python3] Make isolated module py3 compatible
    Bug: 1239059
    Change-Id: I3f214e66ed99d70b2a576d51889e68150b330f16
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/recipes-py/+/3138089
    Commit-Queue: Yiwei Zhang <yiwzhang@google.com>
    Auto-Submit: Rob Mohr <mohrr@google.com>
    Reviewed-by: Yiwei Zhang <yiwzhang@google.com>

--- recipe_modules/isolated/__init__.py
+++ recipe_modules/isolated/__init__.py
@@ -1,3 +1,9 @@
+# Copyright 2018 The LUCI Authors. All rights reserved.
+# Use of this source code is governed under the Apache License, Version 2.0
+# that can be found in the LICENSE file.
+
+PYTHON_VERSION_COMPATIBILITY = 'PY2+3'
+
 DEPS = [
     'properties',
 ]
