74207fa63ab42cbee8d4afcb57de901a8f142ce8
Yuanjun Huang <yuanjunh@google.com>
2021-12-01 22:04:46 +0000

    [autoroll] Run again no matter which type of error is thrown
    New errors are observed to cause the mis-categorization of trivial to
    non-trivial. Loosen the condition to let it retry as long as it fails
    in 1st time.
    
    Bug: 1238528
    Change-Id: I4ff8cf15261ab225b21ebd9ed6804a15d82c5c06
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/recipes-py/+/3308626
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    Commit-Queue: Yuanjun Huang <yuanjunh@google.com>

--- recipe_engine/internal/commands/autoroll/cmd.py
+++ recipe_engine/internal/commands/autoroll/cmd.py
@@ -165,13 +165,14 @@ def process_candidates(recipe_deps, candidates, repos, verbose_json):
 
     write_global_files_to_main_repo(recipe_deps, candidate.repo_spec)
 
+    retcode, output = run_simulation_test(
+        recipe_deps.main_repo, 'run', '--no-docs')
     # TODO(crbug.com/1147793): Remove the retry after py3 migration is done.
-    retcode = output = None
-    for attempt_number in range(2):
+    # Run again as it may be a false error from coverage lib when running and
+    # combining the results from both py2 and py3.
+    if retcode > 0:
       retcode, output = run_simulation_test(
-          recipe_deps.main_repo, 'run', '--no-docs')
-      if not (retcode > 0 and 'CoverageException' in output):
-        break
+        recipe_deps.main_repo, 'run', '--no-docs')
 
     if verbose_json:
       roll_details[i]['recipes_simulation_test'] = {
