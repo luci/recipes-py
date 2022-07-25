4b7ba01b8ce8d4052feff1408188ef7932351b91
Oliver Newman <olivernewman@google.com>
2022-07-25 19:19:36 +0000

    [analyze] Fix py3 incompatibility
    `subprocess.check_output()` returns bytes by default in Python 3, which
    resulted in a TypeError from `os.path.join()`: "Can't mix strings and
    bytes in path components". Set `text=True` to fix this, but only in
    Python 3, since that option didn't exist in Python 2.
    
    Change-Id: Ifdd9edcec84191937160b64a6950c8d34276ae0f
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/recipes-py/+/3784987
    Commit-Queue: Yiwei Zhang <yiwzhang@google.com>
    Commit-Queue: Oliver Newman <olivernewman@google.com>
    Auto-Submit: Oliver Newman <olivernewman@google.com>
    Reviewed-by: Yiwei Zhang <yiwzhang@google.com>

--- recipe_engine/internal/commands/analyze/cmd.py
+++ recipe_engine/internal/commands/analyze/cmd.py
@@ -13,6 +13,8 @@ from google.protobuf import json_format as jsonpb
 
 from PB.recipe_engine.analyze import Input, Output
 
+_PY3 = sys.version_info.major == 3
+
 GIT = 'git.bat' if sys.platform == 'win32' else 'git'
 
 
@@ -30,9 +32,13 @@ def get_git_attribute_files(repo_root):
     GIT, '-C', repo_root, 'ls-files', '--',
     ':(attr:recipes)',
   ]
+  kwargs = {}
+  if _PY3:
+    # os.path.join requires that arguments be strings in Python 3.
+    kwargs['text'] = True
   return [
       os.path.join(repo_root, path) for path in
-      subprocess.check_output(args).splitlines()]
+      subprocess.check_output(args, **kwargs).splitlines()]
 
 
 def analyze(recipe_deps, in_data):
