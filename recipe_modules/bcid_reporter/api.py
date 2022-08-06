d70dec9441f91a8a9d0d9789527a23def02f5370
Akash Mukherjee <akashmukherjee@google.com>
2022-08-06 06:01:27 +0000

    [snoopy] Uprev provenance broker tool.
    This CL delta is a fixed log line, no functional change.
    
    R=jclinton@google.com
    
    Recipe-Nontrivial-Roll: build_limited
    Recipe-Nontrivial-Roll: chrome_release
    Recipe-Nontrivial-Roll: chromiumos
    Recipe-Nontrivial-Roll: infra
    Bug:1269830
    Change-Id: I6f8f5b28a178c5d30518788f91c61b7d1e5a0d1f
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/recipes-py/+/3814613
    Reviewed-by: Jason Clinton <jclinton@chromium.org>
    Commit-Queue: Jason Clinton <jclinton@chromium.org>
    Commit-Queue: Akash Mukherjee <akashmukherjee@google.com>
    Auto-Submit: Akash Mukherjee <akashmukherjee@google.com>

--- recipe_modules/bcid_reporter/api.py
+++ recipe_modules/bcid_reporter/api.py
@@ -8,7 +8,7 @@ from recipe_engine import recipe_api
 # Usage of bcid_reporter recipe_module will have significant downstream impact
 # and to avoid any production outage, we are pinning the latest known good build
 # of the tool here. Upstream changes are intentionally left out.
-_LATEST_STABLE_VERSION = 'git_revision:e8e859be7e6b6b2286a78dce87bb9111deb1ddd9'
+_LATEST_STABLE_VERSION = 'git_revision:557c04a6caeb755ce7e75d234efc8481c924b1a6'
 
 class BcidReporterApi(recipe_api.RecipeApi):
   """API for interacting with Provenance server using the broker tool."""
