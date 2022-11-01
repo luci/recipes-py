8ae554d9e8523d55d62b5a61c59abfc7d68725af
Robert Iannucci <iannucci@chromium.org>
2022-11-01 19:02:42 +0000

    [step] Add better examples to stdio test.
    R=bshai@google.com
    
    Change-Id: Icd4daa2e45ea47b234c159001a6f58e073ceb5d1
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/recipes-py/+/3995151
    Auto-Submit: Robbie Iannucci <iannucci@chromium.org>
    Reviewed-by: Chan Li <chanli@chromium.org>
    Reviewed-by: Benjamin Shai <bshai@google.com>
    Commit-Queue: Chan Li <chanli@chromium.org>

--- recipe_modules/step/tests/stdio.py
+++ recipe_modules/step/tests/stdio.py
@@ -13,8 +13,13 @@ DEPS = [
 def RunSteps(api):
   # Read command's stdout and stderr.
   step_result = api.step('echo', ['echo', 'Hello World'],
-      stdout=api.raw_io.output(),
-      stderr=api.raw_io.output())
+      stdout=api.raw_io.output_text(),
+      stderr=api.raw_io.output_text())
+
+  if step_result.stdout == "stdout":
+    api.step.empty('mocked stdout')
+  if step_result.stderr == "stderr":
+    api.step.empty('mocked stderr')
 
   # Pass stuff to command's stdin, read it from stdout.
   step_result = api.step('cat', ['cat'],
@@ -28,8 +33,34 @@ def RunSteps(api):
       stdout=api.raw_io.output_text('out'),
       step_test_data=(
           lambda: api.raw_io.test_api.stream_output_text('huh\n')))
-  assert step_result.stdout == 'huh\n'
+  if step_result.stdout == 'huh\n':
+    api.step.empty('inline mock')
+  else:
+    api.step.empty('test mock', step_text=step_result.stdout)
 
 
 def GenTests(api):
-  yield api.test('basic')
+  yield api.test(
+      'basic',
+      api.post_check(lambda check, steps: check('mocked stdout') not in steps),
+      api.post_check(lambda check, steps: check('mocked stderr') not in steps),
+      api.post_check(lambda check, steps: check('inline mock') in steps),
+      api.post_check(lambda check, steps: check('test mock') not in steps),
+  )
+
+  yield api.test(
+      'mocking',
+      api.step_data(
+          'echo',
+          stdout=api.raw_io.output_text('stdout'),
+          stderr=api.raw_io.output_text('stderr'),
+      ),
+      api.override_step_data(
+          'automock',
+          stdout=api.raw_io.output_text('OVERRIDE'),
+      ),
+      api.post_check(lambda check, steps: check('mocked stdout') in steps),
+      api.post_check(lambda check, steps: check('mocked stderr') in steps),
+      api.post_check(lambda check, steps: check('inline mock') not in steps),
+      api.post_check(lambda check, steps: check('test mock') in steps),
+  )
