b237d085b98c2adeb7fbe692df13db0b96760609
Rob Mohr <mohrr@google.com>
2023-06-13 20:11:57 +0000

    [url] Nest steps in test
    No changes to test behavior.
    
    Change-Id: I3c65b15cc7748edc07477eeb6878824b453c416b
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/recipes-py/+/4605354
    Auto-Submit: Rob Mohr <mohrr@google.com>
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    Commit-Queue: Rob Mohr <mohrr@google.com>

--- recipe_modules/url/examples/full.py
+++ recipe_modules/url/examples/full.py
@@ -29,81 +29,83 @@ def RunSteps(api):
   assert api.url.unquote('%20foo') == ' foo'
   assert api.url.urlencode({'foo': 'bar'}) == 'foo=bar'
 
-  # get_file
-  dest = api.path['start_dir'].join('download.bin')
-  v = api.url.get_file(TEST_HTTPS_URL, dest,
-                       headers={'Authorization': 'thing'})
-  assert str(v.output) == str(dest)
+  with api.step.nest('get_file'):
+    dest = api.path['start_dir'].join('download.bin')
+    v = api.url.get_file(TEST_HTTPS_URL, dest,
+                         headers={'Authorization': 'thing'})
+    assert str(v.output) == str(dest)
 
-  # get_text (HTTP)
-  v = api.url.get_text(TEST_HTTP_URL, default_test_data='will be overridden')
-  assert v.method == 'GET'
-  assert 'The Chromium Projects' in v.output
-  assert v.size == len(v.output)
-  assert v.status_code == 200
+  with api.step.nest('get_text'):
+    v = api.url.get_text(TEST_HTTP_URL, default_test_data='will be overridden')
+    assert v.method == 'GET'
+    assert 'The Chromium Projects' in v.output
+    assert v.size == len(v.output)
+    assert v.status_code == 200
 
-  v = api.url.get_text(TEST_HTTP_URL, default_test_data='The Chromium Projects')
-  assert 'The Chromium Projects' in v.output
-  assert v.size == len(v.output)
-  assert v.status_code == 200
+    v = api.url.get_text(TEST_HTTP_URL,
+                         default_test_data='The Chromium Projects')
+    assert 'The Chromium Projects' in v.output
+    assert v.size == len(v.output)
+    assert v.status_code == 200
 
-  # get_json (HTTPS)
-  v = api.url.get_json(TEST_JSON_URL, log=True,
-      strip_prefix=api.url.GERRIT_JSON_PREFIX)
-  assert isinstance(v.output, dict)
-  assert v.status_code == 200
+  with api.step.nest('get_json'):
+    v = api.url.get_json(TEST_JSON_URL, log=True,
+                         strip_prefix=api.url.GERRIT_JSON_PREFIX)
+    assert isinstance(v.output, dict)
+    assert v.status_code == 200
 
-  v = api.url.get_json(TEST_JSON_URL,
-      strip_prefix=api.url.GERRIT_JSON_PREFIX,
-      default_test_data={'pants': 'shirt'})
-  assert isinstance(v.output, dict)
-  assert v.status_code == 200
+    v = api.url.get_json(TEST_JSON_URL,
+                         strip_prefix=api.url.GERRIT_JSON_PREFIX,
+                         default_test_data={'pants': 'shirt'})
+    assert isinstance(v.output, dict)
+    assert v.status_code == 200
 
-  # Check error conditions.
-  def raises(fn, exc):
-    raised = None
-    try:
-      fn()
-    except exc as e:
-      raised = e
-    assert raised, 'Did not raise [%s]' % (exc,)
-    return raised
+  with api.step.nest('errors'):
+    # Check error conditions.
+    def raises(fn, exc):
+      raised = None
+      try:
+        fn()
+      except exc as e:
+        raised = e
+      assert raised, 'Did not raise [%s]' % (exc,)
+      return raised
 
-  def test_error():
-    api.url.get_text(TEST_ERROR_URL, step_name='error', transient_retry=4)
-  exc = raises(test_error, api.url.HTTPError)
-  assert exc.response.error_body == '500 Internal Server Error'
+    def test_error():
+      api.url.get_text(TEST_ERROR_URL, step_name='error', transient_retry=4)
+    exc = raises(test_error, api.url.HTTPError)
+    assert exc.response.error_body == '500 Internal Server Error'
 
-  def test_infra_error():
-    with api.context(infra_steps=True):
-      api.url.get_text(TEST_ERROR_URL, step_name='infra error',
-                       transient_retry=False)
-  exc = raises(test_infra_error, api.url.InfraHTTPError)
-  assert exc.response.error_body == '500 Internal Server Error'
+    def test_infra_error():
+      with api.context(infra_steps=True):
+        api.url.get_text(TEST_ERROR_URL, step_name='infra error',
+                         transient_retry=False)
+    exc = raises(test_infra_error, api.url.InfraHTTPError)
+    assert exc.response.error_body == '500 Internal Server Error'
 
-  def test_auth_over_http():
-    api.url.get_text('http://foo/bar/text/error',
-                     headers={'Authorization': 'SECRET'})
-  raises(test_auth_over_http, ValueError)
+    def test_auth_over_http():
+      api.url.get_text('http://foo/bar/text/error',
+                       headers={'Authorization': 'SECRET'})
+    raises(test_auth_over_http, ValueError)
 
-  for bad_cert in TEST_BAD_CERTS:
-    def test_bad_cert():
-      api.url.get_text(bad_cert)
-    raises(test_bad_cert, api.step.StepFailure)
+    for bad_cert in TEST_BAD_CERTS:
+      def test_bad_cert():
+        api.url.get_text(bad_cert)
+      raises(test_bad_cert, api.step.StepFailure)
 
 
 def GenTests(api):
-  def get(url):
-    return 'GET %s' % (url,)
+  def name(prefix, url):
+    return '%s.GET %s' % (prefix, url)
 
   test = (
       api.test('basic') +
-      api.url.text(get(TEST_HTTP_URL), '<html>The Chromium Projects</html>') +
-      api.url.json(get(TEST_JSON_URL), {'is_json': True}) +
-
-      api.url.error('error', 500, body='500 Internal Server Error') +
-      api.url.error('infra error', 500, body='500 Internal Server Error')
+      api.url.text(name('get_text', TEST_HTTP_URL),
+                   '<html>The Chromium Projects</html>') +
+      api.url.json(name('get_json', TEST_JSON_URL), {'is_json': True}) +
+      api.url.error('errors.error', 500, body='500 Internal Server Error') +
+      api.url.error('errors.infra error', 500, body='500 Internal Server Error')
   )
   for bad_cert in TEST_BAD_CERTS:
-    test += api.step_data('GET ' + bad_cert, retcode=1)
+    test += api.step_data(name('errors', bad_cert), retcode=1)
   yield test
